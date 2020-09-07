208ffc7edd6dd0f03433a030af9c847f76d37c15
Balaji G <gbalaji20002000@gmail.com>
2020-09-07 12:55:01 +0530

    Add files via upload

---
+++ HCF of two numbers.py
@@ -0,0 +1,11 @@
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+if x > y:
+    s = y
+else:
+    s = x
+for i in range(1, s+1):
+    if((x % i == 0) and (y % i == 0)):
+        hcf = i
+print"The H.C.F. of", x,"and", y,"is", hcf
