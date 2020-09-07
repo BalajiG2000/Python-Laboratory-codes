4f3f3de98dc243c1f1466931cc259d2fc8629d40
Balaji G <gbalaji20002000@gmail.com>
2020-09-07 12:52:02 +0530

    Add files via upload

---
+++ circle and rectangle area, perimeter calculator.py
@@ -0,0 +1,9 @@
+l=int(input("Enter the length "))
+b=int(input("Enter the breadth "))
+r=int(input("Enter the radius "))
+pi=3.14
+print "area= ",l*b
+print "perimeter= ",2*(l+b)
+print "Area of circle = ",pi*r*r
+print "cicumference = ",2*pi*r
+
