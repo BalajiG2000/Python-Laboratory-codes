a59575f4682c26fda0f32259f226494e7fa4c5aa
Balaji G <gbalaji20002000@gmail.com>
2020-09-07 12:40:38 +0530

    Add files via upload

---
+++ aggregate marks and percentage marks calculator.py
@@ -0,0 +1,9 @@
+print "Enter the marks of the students "
+m=int(raw_input())
+p=int(raw_input())
+c=int(raw_input())
+b=int(raw_input())
+a=int(raw_input())
+s=m+p+c+b+a
+print "The aggregate marks =",s
+print "Percentage=",(s/500.0)*100,"%"
