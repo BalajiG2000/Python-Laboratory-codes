aadcfc38fd4549ee82409fede8ca4d0cd942815b
Balaji G <gbalaji20002000@gmail.com>
2021-04-30 18:21:19 +0530

    Add files via upload

---
+++ lab 8/lab8.4.py
@@ -0,0 +1,11 @@
+def max(a,b,c):
+    if a>b and a>c:
+        print a
+    if b>a and b>c:
+        print b
+    if c>a and c>b:
+        print c
+a=input('enter the 1st number')
+b=input('enter the 2nd number')
+c=input('enter the 3rd number')
+max(a,b,c)
